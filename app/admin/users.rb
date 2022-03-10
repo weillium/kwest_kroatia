8ac15b02633ca9aa9ae334091d88d0db6e333154
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 23:32:26 +0000

    Whitelist columns in User for ActiveAdmin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register User do
 
+ permit_params :email, :password, :first_name, :last_name, :user_type_id, :family_id, :phone_number, :dietary_restrictions, :headshot, :walkup_song, :adventure_score, :nightlife_score, :beachwater_score, :freetime_score
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

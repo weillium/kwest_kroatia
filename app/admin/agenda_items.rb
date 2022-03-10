5bdc4a30ff2b87cd7f2a9a8b59115f5016a428fc
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 23:32:29 +0000

    Whitelist columns in AgendaItem for ActiveAdmin

--- app/admin/agenda_items.rb
+++ app/admin/agenda_items.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register AgendaItem do
 
+ permit_params :start_time, :end_time, :location, :activity, :description, :photo, :leader_id, :is_cyoa
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #

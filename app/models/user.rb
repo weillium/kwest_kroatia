58419133a1770c0a84f01f6ee369ea07b6a74aa0
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 23:32:11 +0000

    Validate presence of users.phone_number

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :phone_number, :presence => true
+
   validates :walkup_song, :presence => true
 
   # Scopes
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe User, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:phone_number) }
+
     it { should validate_presence_of(:walkup_song) }
 
     end

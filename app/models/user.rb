551a4519b52fac931eaad8ae7e76bfa15fa93d4c
anonymous anonymous <will.liao@kellogg.northwestern.edu>
2022-03-10 23:32:11 +0000

    Validate presence of users.last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :last_name, :presence => true
+
   validates :nightlife_score, :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 0 }
 
   validates :phone_number, :presence => true
